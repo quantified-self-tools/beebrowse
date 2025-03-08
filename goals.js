a84120d972bdfd13f4de74dcdf656b95a5fd29ad
Benjamin Fox <benjaminfox@hey.com>
2025-03-08 15:14:00 +0700

    Reset tabindexes when goals are sorted or collapsed

--- goals.js
+++ goals.js
@@ -1,15 +1,22 @@
 import Sorting from './sorting'
-import {
-  getGoalElements,
-  getGoalParentElement,
-  sortGoals
-} from './util'
+import { getGoalElements, getGoalParentElement, sortGoals } from './util'
 
 export function renderGoalList () {
   const elems = getGoalElements()
 
   sortGoals(elems, Sorting.sortBy)
 
+  resetTabIndexes(elems)
+
   const goalParent = getGoalParentElement()
-  elems.forEach(elem => goalParent.append(elem))
+  elems.forEach((elem) => goalParent.append(elem))
+}
+
+function resetTabIndexes (elems) {
+  let tabindex = 1000
+  elems.forEach((elem) => {
+    elem.querySelectorAll(':is(select, input):not(:disabled)').forEach((el) => {
+      el.tabIndex = tabindex++
+    })
+  })
 }
